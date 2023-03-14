bd6e9e105f7b02497fb8d3ec9f9991c852e7510d
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-03-14 18:38:17 -0500

    Return a value from a function

---
+++ ReturnValue.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val greeting = birthdayGreeting()
+    println(greeting)
+}
+
+fun birthdayGreeting(): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
