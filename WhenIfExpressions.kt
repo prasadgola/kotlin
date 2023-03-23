48ad077f94aa8ea373cfe1ef9a28a4094d5e6d38
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-03-23 03:27:09 -0500

    When and If using as Expressions to assign variables

---
+++ WhenIfExpressions.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val trafficLightColor = "Black"
+
+    val message1 = 
+      if (trafficLightColor == "Red") "Stop"
+      else if (trafficLightColor == "Yellow") "Slow"
+      else if (trafficLightColor == "Green") "Go"
+      else "Invalid traffic-light color"
+
+    val message2 = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+
+    println(message1)
+    println(message2)
+}
+
