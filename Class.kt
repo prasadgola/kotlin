66308c1281dc301ab62631967d17051bf310f16c
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-03-25 23:50:49 -0500

    class in kotlin

---
+++ Class.kt
@@ -0,0 +1,21 @@
+class SmartDevice {
+
+    val name = "Android TV"
+    val category = "Entertainment"
+    var deviceStatus = "online"
+
+    fun turnOn(){
+        println("Smart device is turned on.")
+    }
+
+    fun turnOff(){
+        println("Smart device is turned off.")
+    }
+}
+
+fun main(){
+    val smartTvDevice = SmartDevice()
+    println("Device name is: ${smartTvDevice.name}")
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
+}
